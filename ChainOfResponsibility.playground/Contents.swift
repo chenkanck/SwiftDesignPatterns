57061d8db3fd20ab07fa9a7055146c1c05ef4e3f
chenkanck <kan@drchrono.com>
2019-07-25 11:37:44 -0700

    add ChainOfResponsibiity of another version

--- ChainOfResponsibility.playground/Contents.swift
+++ ChainOfResponsibility.playground/Contents.swift
@@ -70,3 +70,129 @@ let request3 = Request(amount: 60, type: "protooooo")
 manager.handle(request: request1)
 manager.handle(request: request2)
 manager.handle(request: request3)
+
+
+/**
+ Swift Design pattern version
+ */
+
+
+struct Message {
+    let from:String
+    let to:String
+    let subject:String
+}
+
+class LocalTransmitter {
+    func sendMessage(_ message: Message) {
+        print("Message to \(message.to) sent locally")
+    }
+}
+
+class RemoteTransmitter {
+    func sendMessage(_ message: Message) {
+        print("Message to \(message.to) sent remotely")
+    }
+}
+
+class PriorityTransmitter {
+    func sendMessage(_ message: Message) {
+        print("Message to \(message.to) sent as priority");
+    }
+}
+
+let messages = [
+    Message(from: "bob@example.com", to: "joe@example.com",
+            subject: "Free for lunch?"),
+    Message(from: "joe@example.com", to: "alice@acme.com",
+            subject: "New Contracts"),
+    Message(from: "pete@example.com", to: "all@example.com",
+            subject: "Priority: All-Hands Meeting"),
+]
+
+let localT = LocalTransmitter();
+let remoteT = RemoteTransmitter();
+let priorityT = PriorityTransmitter();
+
+for msg in messages {
+    if msg.subject.hasPrefix("Priority") {
+        priorityT.sendMessage(msg);
+    } else if let index = msg.from.firstIndex(of: "@") {
+        if msg.to.hasSuffix(msg.from[index..<msg.from.endIndex]) {
+            localT.sendMessage(msg);
+        } else {
+            remoteT.sendMessage(msg);
+        }
+    } else {
+        print("Error: cannot send message to \(msg.from)");
+    }
+}
+
+
+class Transmitter {
+    var nextLink:Transmitter?;
+    required init() {}
+    func sendMessage(_ message:Message) {
+        if (nextLink != nil) {
+            nextLink!.sendMessage(message);
+        } else {
+            print("End of chain reached. Message not sent");
+        }
+    }
+
+    class func matchEmailSuffix(_ message:Message) -> Bool {
+        if let index = message.from.firstIndex(of: "@") {
+            return message.to.hasSuffix(message.from[index..<message.from.endIndex])
+        }
+        return false;
+    }
+
+    class func createChain() -> Transmitter? {
+        let transmitterClasses:[Transmitter.Type] = [
+            PriorityTransmitterV2.self,
+            LocalTransmitterV2.self,
+            RemoteTransmitterV2.self
+        ];
+
+        var link:Transmitter?;
+        for tClass in transmitterClasses.reversed() {
+            let existingLink = link;
+            link = tClass.init();
+            link?.nextLink = existingLink;
+        }
+        return link;
+    }
+}
+
+class LocalTransmitterV2 : Transmitter {
+    override func sendMessage(_ message: Message) {
+        if (Transmitter.matchEmailSuffix(message)) {
+            print("Message to \(message.to) sent locally");
+        } else {
+            super.sendMessage(message);
+        } }
+}
+
+class RemoteTransmitterV2 : Transmitter {
+    override func sendMessage(_ message: Message) {
+        if (!Transmitter.matchEmailSuffix(message)) {
+            print("Message to \(message.to) sent remotely");
+        } else {
+            super.sendMessage(message);
+        } }
+}
+
+class PriorityTransmitterV2 : Transmitter {
+    override func sendMessage(_ message: Message) {
+        if (message.subject.hasPrefix("Priority")) {
+            print("Message to \(message.to) sent as priority");
+        } else {
+            super.sendMessage(message);
+        } }
+}
+
+if let chain = Transmitter.createChain() {
+    for msg in messages {
+        chain.sendMessage(msg);
+    }
+}
